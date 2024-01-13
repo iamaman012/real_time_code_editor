e6d0805364589b5d94f411f5b64d760789b56583
iamaman012 <111675545+iamaman012@users.noreply.github.com>
2024-01-14 03:26:26 +0530

    added: code-compiler

--- src/components/OutputDetails.js
+++ src/components/OutputDetails.js
@@ -1,18 +1,28 @@
 import React from "react";
-import Select from "react-select";
-import { customStyles } from "../constants/customStyles";
-import { languageOptions } from "../constants/languageOptions";
 
-const LanguagesDropdown = ({ onSelectChange }) => {
+const OutputDetails = ({ outputDetails }) => {
   return (
-    <Select
-      placeholder={`Filter By Category`}
-      options={languageOptions}
-      styles={customStyles}
-      defaultValue={languageOptions[0]}
-      onChange={(selectedOption) => onSelectChange(selectedOption)}
-    />
+    <div className="metrics-container mt-4 flex flex-col space-y-3">
+      <p className="text-sm text-[#00ff95]">
+        Status:{" "}
+        <span className="font-semibold px-2 py-1 rounded-md bg-gray-700">
+          {outputDetails?.status?.description}
+        </span>
+      </p>
+      <p className="text-sm text-[#00ff95]">
+        Memory:{" "}
+        <span className="font-semibold px-2 py-1 rounded-md bg-gray-700">
+          {outputDetails?.memory}
+        </span>
+      </p>
+      <p className="text-sm text-[#00ff95]">
+        Time:{" "}
+        <span className="font-semibold px-2 py-1 rounded-md bg-gray-700">
+          {outputDetails?.time}
+        </span>
+      </p>
+    </div>
   );
 };
 
-export default LanguagesDropdown;
+export default OutputDetails;
